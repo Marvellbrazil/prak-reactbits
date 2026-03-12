a7def55252759cada2cd933e0e9aa888ad92997e
Marvello Faisal <marvellbrazil@gmail.com>
2026-03-12 18:53:03 +0700

    Modify LSP server configuration in lspconfig.lua
    Updated the list of LSP servers for configuration.

--- frontend/lspconfig.lua
+++ frontend/lspconfig.lua
@@ -3,8 +3,6 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
 
--- Daftarkan server yang sudah kamu install di Mason tadi
--- ts_ls adalah untuk React/JS, tailwindcss untuk styling
 local servers = { "html", "cssls", "ts_ls", "tailwindcss", "eslint" }
 
 for _, lsp in ipairs(servers) do
